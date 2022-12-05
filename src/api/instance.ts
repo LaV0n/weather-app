638a6570433c9fcf3a7585630476e373f8f60fe8
LaV0n <leonidmedica@gmail.com>
2022-12-05 10:10:21 +0100

    fix:fix bugs

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const APIKey= '25d2f0229435d98d8edbe8e323ee58bd'
 
 export const instance=axios.create({
-    baseURL:'http://api.openweathermap.org',
+    baseURL:'https://api.openweathermap.org',
 })
